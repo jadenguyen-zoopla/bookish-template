fef6495a73041276a30b87cf1e0f2597feaac088
Jade <jade.nguyen@zoopla.co.uk>
2020-08-19 09:55:12 +0100

    added more query pages

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,7 @@
 import express, { response, request } from "express";
 import nunjucks from "nunjucks";
 import sassMiddleware from "node-sass-middleware";
-import {getAllBooks, getAllAuthors, getAllTitles, addNewBook} from "./database"
+import {getAllBooks, getAllAuthors, getAllTitles, addNewBook, deleteBook, getBookById, getAllUsers, getUserBookList} from "./database"
 
 const app = express();
 const port = process.env['PORT'] || 3000;
@@ -37,6 +37,7 @@ app.get("/", (req, res) => {
     res.render('index.html', model);
 })
 
+/* ========= LIST OF BOOKS ====== */
 app.get("/all-books", async (request, response) => {
     const bookRequest = await getAllBooks();
     const model = {
@@ -45,6 +46,21 @@ app.get("/all-books", async (request, response) => {
     response.render('books.html', model);
 });
 
+/* ========= GET BOOK INFO WITH ID ====== */
+app.get("/book-id", async (request, response) => {
+    response.render('bookInfo.html')
+}) 
+
+app.post("/book-id", async (request, response) => {
+    const bookID = parseInt(request.params.id);
+    const model = {
+        book: await getBookById(bookID)
+    };
+    response.render('bookInfo.html', model)
+    
+})
+
+/* ========= LIST OF TITLES ====== */
 app.get("/all-titles", async (request, response) => {
     const titleRequest = await getAllTitles();
     const model = {
@@ -53,6 +69,7 @@ app.get("/all-titles", async (request, response) => {
     response.render('titles.html', model);
 })
 
+/* ========= LIST OF AUTHORS ====== */
 app.get("/all-authors", async (request, response) => {
     const authorRequest = await getAllAuthors();
     const model = {
@@ -61,6 +78,7 @@ app.get("/all-authors", async (request, response) => {
     response.render('authors.html', model);
 })
 
+/* ========= ADD/DELETE BOOK ====== */
 app.get("/add-book", async (request, response) => {
     response.render('addBook.html')
 }) 
@@ -72,6 +90,38 @@ app.post("/add-book", async (request, response) => {
     response.redirect("/all-books");
 });
 
+app.get("/delete-book", async (request, response) => {
+    response.render('deleteBook.html')
+}) 
+
+app.post("/delete-book", async (request, response) => {
+    const delete_book = request.body.id
+    await deleteBook(delete_book)
+    response.redirect("/all-books");
+});
+
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`)
 });
+
+/* ========= LIST OF MEMBERS ====== */
+app.get("/all-users", async (request, response) => {
+    const userRequest = await getAllUsers();
+    const model = {
+        users: userRequest
+    }
+    response.render('users.html', model);
+})
+
+/* ========= GET MEMBER INFO WITH ID ====== */
+app.get("/user-id", async (request, response) => {
+    response.render('userWithBook.html')
+}) 
+
+app.post("/user-id", async (request, response) => {
+    const userID = request.body.id
+    const model = {
+        user: await getUserBookList(userID)
+    };
+    response.render('userWithBook.html', model)
+})
--- src/database.ts
+++ src/database.ts
@@ -1,4 +1,5 @@
 import Knex from "knex";
+import { title } from "process";
 
 const client = Knex ({
     client: 'pg',
@@ -12,29 +13,34 @@ const client = Knex ({
 });
 
 /* ========= LIST OF BOOKS ====== */
-
 export const getAllBooks = () => {
     return client('book')
     .select()
-}
+};
+
+/* ========= GET BOOK WITH ID (DOESNT WORK YET) ====== */
+export const getBookById = (id: number) => {
+    return client.select('*')
+    .from<Book>('book')
+    .where('id', id).first();
+};
 
 /* ========= LIST OF TITLES ====== */
-
 export const getAllTitles = () => {
     return client('book')
     .select('title')
 }
 
 /* ========= LIST OF AUTHORS ====== */
-
 export const getAllAuthors = () => {
     return client('book')
     .select('author')
 }
 
-/* ========= ADD BOOK ====== */
+/* ========= ADD/DELETE BOOK ====== */
 
 interface Book {
+    id: number;
     title: string;
     author: string;
 }
@@ -42,4 +48,32 @@ interface Book {
 export const addNewBook = (book: Book) => {
    return client.insert({title: book.title, author: book.author})
    .into('book')
+}
+
+export const deleteBook = (id: number) => {
+    return client('book')
+   .delete()
+   .where('id', id)
+}
+
+/* ========= LIST OF USERS ====== */
+export const getAllUsers = () => {
+    return client ('member')
+    .select()
+}
+
+// interface Member {
+//     id: number;
+//     name: string;
+//     email: string;
+// }
+
+/* ========= WHICH USER HAS BOOK ====== */
+export const getUserBookList = (id: number) => {
+    return client('member')
+    .select('member.name', 'member.email', 'book.title', 'checked_out_book.check_out_date', 'checked_out_book.return_date')
+    .join('checked_out_book', 'member.id', 'checked_out_book.member_id')
+    .join('book_copy', 'checked_out_book.copy_id', 'book_copy.id')
+    .join('book', 'book_copy.book_id', 'book.id')
+    .where('member.id', id)
 }
